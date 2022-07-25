d6158f101401f159a3eb17b49ac56d83b3fb75e3
Steve Condylios <steve.condylios@gmail.com>
2022-07-25 19:13:26 +1000

    R package to print loop iterations to console at exponentially disparate intervals

---
+++ R/iterator.R
@@ -0,0 +1,82 @@
+#' Conveniently print loop iterations to console
+#'
+#' @description Prints to console the current loop iteration at custom or exponentially
+#' disparate intervals.
+#' @name iteration
+#'
+#' @usage iteration(iterator_name, iteration_values)
+#'
+#' @param iterator_name The name of the loop iterator (e.g. "i")
+#' @param iteration_values An integer vector specifying loop iterations
+#'     (defaults to the sequence 1, 2, 5, 10, 20, 50, 100, 200, 500 ....)
+#'
+#'
+#' @export
+#'
+#' @examples
+#'
+#' # For a loop that would otherwise give no feedback as to where it is up to,
+#' # simply include iteration() to show progress
+#'
+#' for(i in 1:10000) {
+#'   2 * 2
+#'   iteration()
+#' }
+#' # 10
+#' # 20
+#' # 50
+#' # 100
+#' # 200
+#' # 500
+#' # 1,000
+#' # 2,000
+#' # 5,000
+#' # 10,000
+#' # 20,000
+#' # 50,000
+#'
+#'
+#'
+#' # To use an iterator other than 'i' (example: 'page')
+#' for(page in 1:10000) {
+#'   2 * 2
+#'   iteration("page")
+#' }
+#' # 10
+#' # 20
+#' # 50
+#' # 100
+#' # 200
+#' # 500
+#' # 1,000
+#' # 2,000
+#' # 5,000
+#' # 10,000
+#'
+#' # Use customer iteration intervals
+#' for(i in 1:10000) {
+#'   2 * 2
+#'   iteration(iteration_values = seq(0, 1e4, 1e3))
+#' }
+#'
+#' # 1,000
+#' # 2,000
+#' # 3,000
+#' # 4,000
+#' # 5,000
+#' # 6,000
+#' # 7,000
+#' # 8,000
+#' # 9,000
+#' # 10,000
+
+iteration <- function(iterator_name, iteration_values) {
+  if(missing(iteration_values)) {
+    iteration_values <- rep(c(1,2,5), 10) * (10 ^ (sort(rep(1:30, 3))))
+    }
+  if(missing(iterator_name)) { iterator_name <- "i" }
+  iteration <- get(iterator_name)
+  if(any(iteration == iteration_values)) {
+    cat(paste0(format(iteration, big.mark=",", scientific=FALSE), "\n"))
+    }
+}
