18af362c240cd3b81c5eab968fb3b9e5c4255a90
Steve Condylios <steve.condylios@gmail.com>
2022-08-31 02:35:31 +1000

    Add @return (i.e. 'value') to documentation for iteration()

--- R/iterator.R
+++ R/iterator.R
@@ -10,7 +10,9 @@
 #' @param iteration_values An integer vector specifying loop iterations
 #'     (defaults to the sequence 1, 2, 5, 10, 20, 50, 100, 200, 500 ....)
 #'
-#'
+#' @return iteration() is a non-value-returning function. As such, it will not
+#'     return anything, and instead print to console the value representing the
+#'     current loop iteration.
 #' @export
 #'
 #' @examples
